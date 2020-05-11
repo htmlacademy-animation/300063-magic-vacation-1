45c4dea8c38353f44be8b9fbb4607c1b98015184
kvezal <muckam49@mail.ru>
2020-05-11 14:52:22 +0800

    fix(loading): eventListener delete before loading page (module1-task2)

--- source/js/modules/loading.js
+++ source/js/modules/loading.js
@@ -1,9 +1,10 @@
 export default () => {
-  window.addEventListener(`load`, () => {
+  const loadPageHandler = () => {
     const page = document.querySelector(`.page`);
     const isLoadingPage = page.classList.contains(`page--loading`);
     if (isLoadingPage) {
       page.classList.remove(`page--loading`);
     }
-  });
+  };
+  window.addEventListener(`load`, loadPageHandler, {once: true});
 };
