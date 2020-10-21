a3acf638aa4a9f1e05a56ebb566ebc703303016b
Kvezal <muckam49@mail.ru>
2020-10-21 10:27:46 +0800

    feat: intro title animation added (module2-task2)

--- source/js/modules/intro.js
+++ source/js/modules/intro.js
@@ -1,29 +1,41 @@
 import AccentTypographyBuild from './accent-typography-build';
 
 const TITLE_ANIMATION_DELAY = 500;
+const DATE_ANIMATION_DELAY = 1500;
 const TITLE_LETTER_ANIMATION_DURATION = 500;
-const TITLE_DELAY_INCREMENT = 50;
+const LETTER_DELAY_INCREMENT = 50;
 
 export default () => {
-  const accentTypographyBuild = new AccentTypographyBuild({
+  const titleTypography = new AccentTypographyBuild({
     selector: `.intro__title`,
     classForWordContainer: `intro__title-word`,
     classForActivate: `active`,
     property: `transform`,
     duration: TITLE_LETTER_ANIMATION_DURATION,
-    delayIncrement: TITLE_DELAY_INCREMENT,
+    delayIncrement: LETTER_DELAY_INCREMENT,
+  });
+
+  const dateTypography = new AccentTypographyBuild({
+    selector: `.intro__date`,
+    classForWordContainer: `intro__date-word`,
+    classForActivate: `active`,
+    property: `transform`,
+    duration: TITLE_LETTER_ANIMATION_DURATION,
+    delayIncrement: LETTER_DELAY_INCREMENT,
   });
 
   const showTitle = () => {
     const hash = window.location.hash.slice(1);
     if (hash === `top`) {
-      accentTypographyBuild.run(TITLE_ANIMATION_DELAY);
+      titleTypography.run(TITLE_ANIMATION_DELAY);
+      dateTypography.run(DATE_ANIMATION_DELAY);
     }
   };
   window.addEventListener(`hashchange`, (event) => {
     const oldHash = event.oldURL.split(`#`).pop();
     if (oldHash === `top`) {
-      accentTypographyBuild.cancel();
+      titleTypography.cancel();
+      dateTypography.cancel();
     }
     showTitle();
   });
